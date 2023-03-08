e56ce8608b4f343e92a55c51de746621ac5db38f
Wagner Santos <wagnerstss@gmail.com>
2023-03-08 18:13:41 -0300

    Fibonacci calculate

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+
+
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número: ");
+
+            int numero = int.Parse(Console.ReadLine());
+
+            
+            
+                int n1 = 0, n2 = 1, n3 = 0;
+                while (numero > n3)
+                {
+                    n3 = n1 + n2;
+                    n1 = n2;
+                    n2 = n3;
+                }
+                if (numero == 0)
+                {
+                    Console.WriteLine("Este número pertence ao Fibonacci");
+                }
+
+                else if (numero == n3)
+                {
+                    Console.WriteLine("Este número pertence ao Fibonacci");
+                }
+                else
+                {
+                    Console.WriteLine("Este número não pertence ao Fibonacci");
+                }
+            
+           
+            
+        }
+    }
+}
